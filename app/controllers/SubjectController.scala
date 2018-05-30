e6e46e14850cab37a240fe89b0ba9e42493b70b8
Marcus Mu <chunkhang@gmail.com>
2018-05-30 15:52:20 +0800

    Add subject controller

---
+++ app/controllers/SubjectController.scala
@@ -0,0 +1,33 @@
+package controllers
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+import play.api.mvc._
+import reactivemongo.bson.BSONObjectID
+import actions._
+import models._
+
+class SubjectController @Inject()(
+  cc: ControllerComponents,
+  authenticatedAction: AuthenticatedAction,
+  subjectRepo: SubjectRepository
+)(
+  implicit ec: ExecutionContext
+) extends AbstractController(cc) {
+
+  def edit(id: BSONObjectID) = authenticatedAction.async { implicit request =>
+    subjectRepo.read(id).map { maybeSubject =>
+      maybeSubject match {
+        case Some(subject) =>
+          Ok("!")
+        case None =>
+          Redirect(routes.ClassController.index())
+      }
+    }
+  }
+
+  def update(id: BSONObjectID) = authenticatedAction { implicit request =>
+    Ok("!")
+  }
+
+}
