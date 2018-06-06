bd053ad24c694e2359b55191db57e5935844cd45
Marcus Mu <chunkhang@gmail.com>
2018-06-06 22:31:42 +0800

    Different event sources

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -2,77 +2,66 @@
 
 export function calendar() {
 
+  // Event sources
+  var sources = {
+    month: {
+      url: "/calendar/month/events",
+      type: "GET",
+      error: handleEventSourceError
+    },
+    agendaWeek: {
+      url: "/calendar/week/events",
+      type: "GET",
+      error: handleEventSourceError
+    },
+    listWeek: {
+      url: "/calendar/list/events",
+      type: "GET",
+      error: handleEventSourceError
+    },
+  };
   // Initialize calendar
   var calendar = $("#calendar");
-  initializeCalender();
+  calendar.fullCalendar({
+    schedulerLicenseKey: "CC-Attribution-NonCommercial-NoDerivatives",
+    eventSources: [
+      sources.month,
+      sources.agendaWeek,
+      sources.listWeek
+    ],
+    header: {
+      left: "month,agendaWeek,listWeek",
+      center: "title"
+    },
+    buttonText: {
+      month: "Month",
+      agendaWeek: "Week",
+      listWeek: "List",
+      today: "Today"
+    },
+    nowIndicator: true,
+    businessHours: {
+      dow: [1, 2, 3, 4, 5, 6],
+      start: "08:00",
+      end: "18:00"
+    },
+    minTime: "08:00:00",
+    maxTime: "18:00:00",
+    contentHeight: "auto",
+    viewRender: handleViewRender
+  });
 
-  function initializeCalender() {
-    calendar.fullCalendar({
-      schedulerLicenseKey: "CC-Attribution-NonCommercial-NoDerivatives",
-      eventSources: [
-        {
-          url: "/calendar/events",
-          type: "GET",
-          error: function() {
-            console.log("Failed to fetch calendar events");
-          }
-        }
-      ],
-      header: {
-        left: "month,agendaWeek,listWeek",
-        center: "title"
-      },
-      buttonText: {
-        month: "Month",
-        agendaWeek: "Week",
-        listWeek: "List",
-        today: "Today"
-      },
-      nowIndicator: true,
-      businessHours: {
-        dow: [1, 2, 3, 4, 5, 6],
-        start: "08:00",
-        end: "18:00"
-      },
-      minTime: "08:00:00",
-      maxTime: "18:00:00",
-      contentHeight: "auto",
-      eventAfterRender: handleDoneRenderEvent,
-      eventAfterAllRender: handleDoneRenderAllEvents,
-      dayClick: handleClickDay
-    });
+  function handleEventSourceError() {
+    console.log("Failed to fetch calendar events");
   }
 
-  function handleDoneRenderEvent(event, element) {
-    // Set popover content
-    $(element).attr("title", event.description);
-    $(element).attr("data-content", "Hello World!");
-    $(element).attr("data-trigger", "focus");
-    $(element).attr("tabindex", 0);
-  }
-
-  function handleDoneRenderAllEvents() {
-    // Initalize popovers
-    var events = $(".fc-event");
-    events.popover({
-      placement: "bottom",
-      html: true
-    });
-    events.on("show.bs.popover", function() {
-      console.log("Show!");
-      popoverShowing = true;
-    });
-    events.on("hide.bs.popover", function() {
-      console.log("Hide!");
-      popoverShowing = false;
-    });
-  }
-
-  var popoverShowing = false;
-  function handleClickDay() {
-    // Hide popovers
-    if (popoverShowing) {
-      $(".fc-event").popover("hide");
+  var lastView = "month";
+  function handleViewRender(view) {
+    if (view.name != lastView) {
+      // Change event source
+      calendar.fullCalendar("removeEventSources");
+      calendar.fullCalendar("addEventSource", sources[view.name]);
+      lastView = view.name;
     }
   }
 
--- app/controllers/CalendarController.scala
+++ app/controllers/CalendarController.scala
@@ -18,91 +18,123 @@ class CalendarController @Inject()(
   implicit ec: ExecutionContext
 ) extends AbstractController(cc) {
 
+  case class EventData(
+    subjectItem: Subject,
+    classItem: Class,
+    venue: String,
+    start: String,
+    end: String
+  )
+
   case class Event(
     title: String,
-    allDay: Boolean,
     start: String,
-    end: String,
-    description: String
+    end: String
   )
 
   implicit val eventWrites: Writes[Event] = (
     (JsPath \ "title").write[String] and
-    (JsPath \ "allDay").write[Boolean] and
     (JsPath \ "start").write[String] and
-    (JsPath \ "end").write[String] and
-    (JsPath \ "description").write[String]
+    (JsPath \ "end").write[String]
   )(unlift(Event.unapply))
 
   def index = userAction { implicit request =>
     Ok(views.html.calendar.index())
   }
 
-  def events(start: String, end: String) = userAction.async {
+  def events(view: String, start: String, end: String) = userAction.async {
     implicit request =>
       val email = request.user.email
-      getEvents(email).map { events =>
-        // Filter events based on queried date range
-        val startInteger = utils.totalDays(start)
-        val endInteger = utils.totalDays(end)
-        val filteredEvents = events.filter { event =>
-          utils.totalDays(event.start) >= startInteger &&
-          utils.totalDays(event.end) <= endInteger
+      getEventData(email).map { maybeEventData =>
+        var events = List[Event]()
+        maybeEventData match {
+          case Some(eventData) =>
+            view match {
+              case "month" =>
+                events = eventData.map { data =>
+                  Event(
+                    title = "Month",
+                    start = data.start,
+                    end = data.end
+                  )
+                }
+              case "week" =>
+                events = eventData.map { data =>
+                  Event(
+                    title = "Week",
+                    start = data.start,
+                    end = data.end
+                  )
+                }
+              case "list" =>
+                events = eventData.map { data =>
+                  Event(
+                    title = "List",
+                    start = data.start,
+                    end = data.end
+                  )
+                }
+              case _ =>
+            }
+            // Filter events based on queried date range
+            val startInteger = utils.totalDays(start)
+            val endInteger = utils.totalDays(end)
+            events = events.filter { event =>
+              utils.totalDays(event.start) >= startInteger &&
+              utils.totalDays(event.end) <= endInteger
+            }
+            Ok(Json.toJson(events))
+          case None =>
+            Ok(Json.toJson(events))
         }
-        Ok(Json.toJson(filteredEvents))
       }
   }
 
-  // Get all classes as calendar events
-  def getEvents(email: String): Future[List[Event]] = {
+  // Get add data required to create calendar event for classes
+  def getEventData(email: String):
+    Future[Option[List[EventData]]] = {
     // Find subjects
-    var events = ListBuffer[Event]()
     classController.getClasses(email).map { maybeSubjectMap =>
       maybeSubjectMap match {
         case Some(subjectMap) =>
           // Flatten map to get class with subject and venue
-          var classItems = ListBuffer[(Subject, Class, Option[String])]()
+          var classes = ListBuffer[(Subject, Class, Option[String])]()
           subjectMap.foreach { item =>
             val (subject, classTuples) = item
             classTuples.foreach { tuple =>
               val (class_, venue) = tuple
               val individualClass = (subject, class_, venue)
-              classItems += individualClass
+              classes += individualClass
             }
           }
           // Only need classes with details
-          classItems = classItems.filter { classItem =>
-            classItem._2.day.isDefined
-          }
-          // Create events
-          var events = ListBuffer[Event]()
-          classItems.toList.map { classItem =>
-            val (subject, class_, venue) = classItem
+          val detailedClasses: List[(Subject, Class, String)] =
+            classes.toList.filter { classItem =>
+              classItem._2.day.isDefined
+            } map { classItem =>
+              (classItem._1, classItem._2, classItem._3.get)
+            }
+          // Convert classes to event data
+          var eventData = ListBuffer[EventData]()
+          detailedClasses.map { tuple =>
+            var (subject, class_, venue_) = tuple
             // First day of class from start date
             val firstDate = utils.firstDate(subject.semester, class_.day.get)
             // Repeat classes weekly until end date
-            val dates =
-              utils.weeklyDates(firstDate, subject.endDate.get)
+            val dates = utils.weeklyDates(firstDate, subject.endDate.get)
             dates.foreach { date =>
-              events += Event(
-                title =
-                  s"${subject.code} " +
-                  s"(${class_.category(0)}${class_.group})",
-                allDay = false,
+              eventData += EventData(
+                subjectItem = subject,
+                classItem = class_,
+                venue = venue_,
                 start = utils.momentTime(date, class_.startTime.get),
                 end = utils.momentTime(date, class_.endTime.get),
-                description = "test"
-                  // s"${subject.title.map(_).getOrElse("")}" +
-                  // |${subject.code}${subject.title.map(": " + _).getOrElse("")}
-                  // |${class_.category}
-                  // |Group ${class_.group}
-                  // |${venue.get}
               )
             }
           }
-          events.toList
+          Some(eventData.toList)
         case None =>
-          events.toList
+          None
       }
     }
   }
